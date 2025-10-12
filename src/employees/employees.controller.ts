4acfead3ee648f3018f4b605c137f7a4de9c92fe
Eyob Simachew <eyob.ugr-1326-16@aau.edu.et>
2025-10-12 22:08:49 +0300

    Learning about pipe and validation and also dependency injection in detail

--- src/employees/employees.controller.ts
+++ src/employees/employees.controller.ts
@@ -17,7 +17,6 @@ import { Throttle, SkipThrottle } from '@nestjs/throttler';
 import { CreateEmployeeDto } from './DTO/create-employee.dto.js';
 import { updateEmployeeDto } from './DTO/update-employee.dto.js';
 
-@SkipThrottle()
 @Controller('employees')
 export class EmployeesController {
   constructor(private readonly employeesService: EmployeesService) {}
@@ -27,13 +26,11 @@ export class EmployeesController {
     return this.employeesService.create(createEmployeeDto);
   }
 
-  @SkipThrottle()
   @Get()
   findAll(@Query('role') role?: 'INTERN' | 'ADMIN' | 'ENGINEER') {
     return this.employeesService.findAll(role);
   }
 
-  @Throttle({ short: { ttl: 60000, limit: 1 } })
   @Get(':id')
   findOne(@Param('id', ParseIntPipe) id: number) {
     return this.employeesService.findOne(id);
