25fef5d7eb1fd3c12f665b3e8d2125499be8d3a2
Eyob Simachew <eyob.ugr-1326-16@aau.edu.et>
2025-10-16 11:57:40 +0300

    Adding caching and learning about interceptors

--- src/common/interceptors/general.interceptor.ts
+++ src/common/interceptors/general.interceptor.ts
@@ -10,12 +10,6 @@ import { catchError, map, Observable, tap, throwError } from 'rxjs';
 @Injectable()
 export class GeneralInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
-    const request = context.switchToHttp().getRequest<Request>();
-    const method = request.method;
-    const url = request.url;
-
-    console.log(`Incoming Request: ${method} ${url} with the ip ${request.ip}`);
-
     return next.handle().pipe(
       map((data) => {
         return { success: true, data };
